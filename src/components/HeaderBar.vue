c5588f2ad1b77cdd9d4d5ddf57f375cf3fe4ffbb
Illia Prokopenko <nobiqp@MacSwin-Air.local>
2025-11-04 19:29:36 -0600

    feat: update app title and icon

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { RouterLink, useRoute } from 'vue-router';
-import HeaderNav from './HeaderNav.vue';
 import { computed } from 'vue';
+import HeaderNav from './HeaderNav.vue';
+import logo from '@/assets/images/logo/logo.png';
 
 const route = useRoute();
 
@@ -12,7 +13,8 @@ const hiddenNav = computed(() => route.path === '/sign-in' || route.path === '/s
 	<header class="header">
 		<div class="header__inner">
 			<RouterLink data-test-id="header-logo" to="/" class="header__logo">
-				Travel App
+				Cat Travel
+				<img :src="logo" alt="Cat Travel Logo" class="logo__image" />
 			</RouterLink>
 			<HeaderNav v-if="!hiddenNav" />
 		</div>
